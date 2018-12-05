8974a0dba61b5572eb81715f62e1e7019a9b9c61
Bogdan Habic <habicbogdan@gmail.com>
2018-12-05 09:06:50 +0100

    Remove log level logging when the --debug option isn't passed.

--- commands/root.go
+++ commands/root.go
@@ -2,6 +2,7 @@ package commands
 
 import (
 	"flag"
+	"fmt"
 	"os"
 
 	"github.com/sirupsen/logrus"
@@ -11,12 +12,18 @@ import (
 
 var debugMode bool
 
+type TenderlyStandardFormatter struct {
+}
+
+func (t TenderlyStandardFormatter) Format(entry *logrus.Entry) ([]byte, error) {
+	return []byte(fmt.Sprintf("%s\n", entry.Message)), nil
+}
+
 func init() {
 	flag.Usage = printHelp
 	cobra.OnInitialize(initConfig)
 
 	rootCmd.PersistentFlags().BoolVar(&debugMode, "debug", false, "Turn on debug level logging.")
-
 }
 
 func Execute() {
@@ -35,8 +42,15 @@ var rootCmd = &cobra.Command{
 }
 
 func initConfig() {
+	logrus.SetFormatter(&logrus.TextFormatter{
+		DisableLevelTruncation: true,
+	})
+	if !debugMode {
+		logrus.SetFormatter(&TenderlyStandardFormatter{})
+	}
 	if debugMode {
 		logrus.SetLevel(logrus.DebugLevel)
+		logrus.SetReportCaller(true)
 	}
 
 	config.Init()
