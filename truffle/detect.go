dbb9771810c083b4cfcc7364c9524150d10eadde
Bogdan Habic <habicbogdan@gmail.com>
2018-12-11 11:07:18 +0100

    Added truffle project detection when logging in.

---
+++ truffle/detect.go
@@ -0,0 +1,92 @@
+package truffle
+
+import (
+	"github.com/sirupsen/logrus"
+	"os"
+	"os/exec"
+	"os/user"
+	"path"
+	"runtime"
+	"strings"
+)
+
+var truffleFolders = []string{
+	"build",
+	"contracts",
+	"migrations",
+}
+
+func FindTruffleDirectories() []string {
+	if runtime.GOOS != "darwin" {
+		return nil
+	}
+
+	cmd := exec.Command("/bin/sh", "-c", "command -v mdfind")
+	if err := cmd.Run(); err != nil {
+		return nil
+	}
+
+	data, err := exec.Command(
+		"mdfind",
+		"-onlyin",
+		getHomeDir(),
+		"kMDItemDisplayName == truffle*.js",
+	).CombinedOutput()
+
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"error":  err,
+			"output": string(data),
+		}).Debug("Couldn't find truffle directories")
+		return nil
+	}
+
+	possibleDirectories := strings.Split(string(data), "\n")
+
+	directories := map[string]bool{}
+
+	for _, possibleDirectory := range possibleDirectories {
+		if strings.Contains(possibleDirectory, "node_modules") {
+			continue
+		}
+
+		dir := path.Dir(possibleDirectory)
+		if !checkIfTruffleStructure(dir) {
+			continue
+		}
+
+		directories[dir] = true
+	}
+
+	var result []string
+
+	for dir := range directories {
+		if dir == "." {
+			continue
+		}
+
+		result = append(result, dir)
+	}
+
+	return result
+}
+
+func checkIfTruffleStructure(directory string) bool {
+	for _, truffleFolder := range truffleFolders {
+		folderPath := path.Join(directory, truffleFolder)
+		if _, err := os.Stat(folderPath); err != nil {
+			return false
+		}
+	}
+
+	return true
+}
+
+func getHomeDir() string {
+	usr, err := user.Current()
+	if err != nil {
+		return "~"
+	}
+
+	return usr.HomeDir
+}
