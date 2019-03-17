b2a1e418b3caa44ce50054b8044f2ca403a9796a
Bogdan Habic <habicbogdan@gmail.com>
2019-03-17 12:43:02 +0100

    Move api domain to .dev

--- rest/client/client.go
+++ rest/client/client.go
@@ -14,7 +14,7 @@ import (
 )
 
 func Request(method, path string, body []byte) io.Reader {
-	apiBase := "http://api.tenderly.love"
+	apiBase := "http://api.tenderly.dev"
 	if alternativeApiBase := config.MaybeGetString("api_base"); len(alternativeApiBase) != 0 {
 		apiBase = alternativeApiBase
 	}
