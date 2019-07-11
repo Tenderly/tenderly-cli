880a1b006e18d11ac14c1badea5f844a7bb1ac42
Bogdan Habic <habicbogdan@gmail.com>
2019-07-11 12:13:03 +0200

    Improved messaging when checking for updates and added an option to display custom update messages from GitHub.

--- commands/check_updates.go
+++ commands/check_updates.go
@@ -2,7 +2,9 @@ package commands
 
 import (
 	"encoding/json"
+	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
+	"github.com/tenderly/tenderly-cli/userError"
 	"io/ioutil"
 	"math/rand"
 	"net/http"
@@ -18,8 +20,24 @@ import (
 )
 
 type releaseResult struct {
-	Name   string `json:"name"`
-	NodeId string `json:"node_id"`
+	Name   string         `json:"name"`
+	NodeId string         `json:"node_id"`
+	Assets []releaseAsset `json:"assets"`
+}
+
+type releaseAsset struct {
+	Name               string `json:"name"`
+	BrowserDownloadUrl string `json:"browser_download_url"`
+}
+
+type releaseCliMessage struct {
+	Format string                  `json:"format"`
+	Parts  []releaseCliMessagePart `json:"parts"`
+}
+
+type releaseCliMessagePart struct {
+	Text       string   `json:"text"`
+	Formatting []string `json:"formatting"`
 }
 
 var versionAlreadyChecked bool
@@ -52,7 +70,13 @@ func CheckVersion(force bool) {
 
 	response, err := http.Get("https://api.github.com/repos/tenderly/tenderly-cli/releases")
 
-	if err != nil {
+	if err != nil || (response != nil && response.StatusCode != 200) {
+		if force {
+			userError.LogErrorf("failed creating github releases request: %s", userError.NewUserError(
+				err,
+				"\nFailed fetching newest releases from GitHub. Please try again.\n",
+			))
+		}
 		return
 	}
 
@@ -60,6 +84,12 @@ func CheckVersion(force bool) {
 
 	contents, err := ioutil.ReadAll(response.Body)
 	if err != nil {
+		if force {
+			userError.LogErrorf("failed reading github releases request: %s", userError.NewUserError(
+				err,
+				"\nFailed parsing latest releases from GitHub. Please try again.\n",
+			))
+		}
 		return
 	}
 
@@ -67,31 +97,64 @@ func CheckVersion(force bool) {
 	err = json.Unmarshal(contents, &result)
 
 	if err != nil || len(result) == 0 {
+		if force {
+			userError.LogErrorf("error unmarshaling github releases: %s", userError.NewUserError(
+				err,
+				"\nFailed parsing latest releases from GitHub. Please try again.\n",
+			))
+		}
 		return
 	}
 
 	sort.Slice(result, func(i, j int) bool {
 		v1, err := version.NewVersion(result[i].Name)
 		if err != nil {
-			return true
+			return false
 		}
 
 		v2, err := version.NewVersion(result[j].Name)
+		if err != nil {
+			return true
+		}
 
 		return v1.GreaterThan(v2)
 	})
 
 	currentVersion, err := version.NewVersion(CurrentCLIVersion)
 	if err != nil {
+		if force {
+			userError.LogErrorf("cannot parse current cli version: %s", userError.NewUserError(
+				err,
+				aurora.Sprintf(
+					"\nCannot parse the current version of the Tenderly CLI. Please follow the installation steps described at %s to re-install the CLI.\n",
+					aurora.Bold(aurora.Green("https://github.com/Tenderly/tenderly-cli#installation")),
+				),
+			))
+		}
 		return
 	}
 
 	newestVersion, err := version.NewVersion(result[0].Name)
 	if err != nil {
+		if force {
+			userError.LogErrorf("cannot parse newest cli version: %s", userError.NewUserError(
+				err,
+				aurora.Sprintf(
+					"\nCannot parse the newest version of the Tenderly CLI. Please follow the installation steps described at %s to re-install the CLI.\n",
+					aurora.Bold(aurora.Green("https://github.com/Tenderly/tenderly-cli#installation")),
+				),
+			))
+		}
 		return
 	}
 
 	if !newestVersion.GreaterThan(currentVersion) {
+		if force {
+			logrus.Info(aurora.Sprintf(
+				"\nYou are already running the newest version of the Tenderly CLI: %s.\n",
+				aurora.Bold(aurora.Green(CurrentCLIVersion)),
+			))
+		}
 		return
 	}
 
@@ -100,26 +163,34 @@ func CheckVersion(force bool) {
 	switch runtime.GOOS {
 	case "darwin":
 		updateCommand = getMacOSInstallationCommand()
-		break
 	case "linux":
+		fallthrough
 	default:
 		updateCommand = aurora.Sprintf("%s", aurora.Bold(aurora.Green("curl https://raw.githubusercontent.com/Tenderly/tenderly-cli/master/scripts/install-linux.sh | sh")))
 	}
 
 	logrus.Info(
-		aurora.Sprintf("\nYou are running version %s of the Tenderly CLI. To update to the newest version (%s) please run the following command:\n\n%s\n\n",
+		aurora.Sprintf("\nYou are running version %s of the Tenderly CLI. To update to the newest version (%s) please follow the instructions below:\n\n%s\n",
 			aurora.Bold(aurora.Green(CurrentCLIVersion)),
 			aurora.Bold(aurora.Green(result[0].Name)),
 			updateCommand,
 		),
 	)
+
+	cliMessage, err := getCliMessage(result[0])
+	if err != nil || cliMessage == "" {
+		logrus.Debugf("error fetching cli message: %s", err)
+		return
+	}
+
+	logrus.Infof("Below are the notes for this release:\n\n%s", cliMessage)
 }
 
 func getMacOSInstallationCommand() string {
 	path, err := os.Executable()
 
 	defaultMessage := aurora.Sprintf("If you installed the CLI via Homebrew you can update it by running:\n\n%s\n\n"+
-		"or if you installed it via the installation script you can just run the installation script again:\n\n%s",
+		"Alternatively, if you installed the CLI via the installation script, you can update your installation by running the same script again:\n\n%s",
 		aurora.Bold(aurora.Green("brew update && brew upgrade tenderly")),
 		aurora.Bold(aurora.Green("curl https://raw.githubusercontent.com/Tenderly/tenderly-cli/master/scripts/install-macos.sh | sh")),
 	)
@@ -135,12 +206,69 @@ func getMacOSInstallationCommand() string {
 	}
 
 	if strings.Contains(link, "Cellar") {
-		return aurora.Sprintf("It seems you installed the CLI via Homebrew, so you can update it by running:\n\n%s\n\n",
+		return aurora.Sprintf("It seems you installed the CLI via Homebrew, so you can update it by running:\n\n%s",
 			aurora.Bold(aurora.Green("brew update && brew upgrade tenderly")),
 		)
 	}
 
-	return aurora.Sprintf("It seems you installed the CLI via the installation script, so you can update it by running:\n\n%s\n\n",
+	return aurora.Sprintf("It seems you installed the CLI via the installation script, so you can update it by running:\n\n%s",
 		aurora.Bold(aurora.Green("curl https://raw.githubusercontent.com/Tenderly/tenderly-cli/master/scripts/install-macos.sh | sh")),
 	)
+}
+
+func getCliMessage(release releaseResult) (string, error) {
+	var assetUrl string
+	for _, asset := range release.Assets {
+		if asset.Name == "cli-message.json" {
+			assetUrl = asset.BrowserDownloadUrl
+			break
+		}
+	}
+
+	if assetUrl == "" {
+		return "", errors.New("couldn't find cli-message.json in asset list")
+	}
+
+	response, err := http.Get(assetUrl)
+
+	if err != nil {
+		return "", errors.Wrap(err, "get cli-message.json")
+	}
+
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return "", errors.Wrap(err, "read cli-message.json")
+	}
+
+	var cliMessage releaseCliMessage
+
+	err = json.Unmarshal(body, &cliMessage)
+	if err != nil {
+		return "", errors.Wrap(err, "unmarshal cli-message.json")
+	}
+
+	var args []interface{}
+
+	for _, part := range cliMessage.Parts {
+		formattedPart := aurora.Gray(part.Text)
+
+		for i := len(part.Formatting) - 1; i >= 0; i-- {
+			switch part.Formatting[i] {
+			case "bold":
+				formattedPart = aurora.Bold(formattedPart)
+			case "green":
+				formattedPart = aurora.Green(formattedPart)
+			case "red":
+				formattedPart = aurora.Red(formattedPart)
+			}
+		}
+
+		args = append(args, formattedPart)
+	}
+
+	message := aurora.Sprintf(cliMessage.Format, args...)
+
+	return message, nil
 }
