5916540f2e52035f3c76eea2d3d7a30d71eaf2dc
nebojsa.urosevic <nebojsa94urosevic@gmail.com>
2020-03-26 10:35:46 +0100

    Fixed api base.

--- rest/client/client.go
+++ rest/client/client.go
@@ -14,7 +14,7 @@ import (
 )
 
 func Request(method, path string, body []byte) io.Reader {
-	apiBase := "http://127.0.0.1:8080"
+	apiBase := "https://api.tenderly.dev"
 	if alternativeApiBase := config.MaybeGetString("api_base"); len(alternativeApiBase) != 0 {
 		apiBase = alternativeApiBase
 	}
