faa7105a3de87c19f3a8aa43e925498400866b16
Filip Petrovic <filip.petroviccc@gmail.com>
2020-08-19 16:58:49 +0200

    added logic for error if whoami fails

--- rest/call/user.go
+++ rest/call/user.go
@@ -2,6 +2,7 @@ package call
 
 import (
 	"encoding/json"
+	"github.com/sirupsen/logrus"
 	"github.com/tenderly/tenderly-cli/rest/payloads"
 
 	"github.com/tenderly/tenderly-cli/model"
@@ -20,6 +21,18 @@ func (rest *UserCalls) User() (*model.User, error) {
 	response := client.Request("GET", "api/v1/user", nil)
 
 	err := json.NewDecoder(response).Decode(&userResponse)
+	if err != nil {
+		logrus.Debug("failed parsing user response")
+		return nil, err
+	}
+
+	if userResponse.Error != nil {
+		logrus.Debug("failed fetching user data ", logrus.Fields{
+			"error_message": userResponse.Error.Message,
+		})
+
+		return nil, userResponse.Error
+	}
 
 	return userResponse.User, err
 }
--- rest/payloads/userPayloads.go
+++ rest/payloads/userPayloads.go
@@ -3,5 +3,6 @@ package payloads
 import "github.com/tenderly/tenderly-cli/model"
 
 type UserResponse struct {
-	User *model.User `json:"user"`
+	User  *model.User `json:"user"`
+	Error *ApiError   `json:"error"`
 }
