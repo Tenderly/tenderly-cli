ad312048c5baaca4f0966af4efd0bb65f7cd17b0
l2cup <uros@tenderly.co>
2022-01-11 16:41:38 +0100

    Fix go vet errors.

--- ethereum/parity/types.go
+++ ethereum/parity/types.go
@@ -520,8 +520,8 @@ func Walk(vmt *VmTrace) []*VmState {
 type Action struct {
 	CallType        string          `json:"callType"`
 	Hash            *common.Hash    `json:"hash"`
-	ParentHash      *common.Hash    `json:"hash"`
-	TransactionHash *common.Hash    `json:"hash"`
+	ParentHash      *common.Hash    `json:"parent_hash"`
+	TransactionHash *common.Hash    `json:"transaction_hash"`
 	From            common.Address  `json:"from"`
 	To              common.Address  `json:"to"`
 	Input           hexutil.Bytes   `json:"input"`
